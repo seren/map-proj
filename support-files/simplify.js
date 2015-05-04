ea35c0d022a8de54fee06a4af59acdf244d2379c
Seren <git@seren.net>
2015-05-04 16:57:03 +0200

    more

--- support-files/simplify.js
+++ support-files/simplify.js
@@ -85,6 +85,12 @@ function area(t) {
   return Math.abs((t[0][0] - t[2][0]) * (t[1][1] - t[0][1]) - (t[0][0] - t[1][0]) * (t[2][1] - t[0][1]));
 }
 
+
+
+
+
+
+
 function minHeap() {
   var heap = {},
       array = [];
