62d446cb17b05f8bc583711bad286acf2dd0b061
Seren <git@seren.net>
2015-11-13 19:30:30 -0500

    minor: missing semicolons and changed obj property references to dot-notation

--- js/illumap-node.js
+++ js/illumap-node.js
@@ -1,13 +1,14 @@
 var Node = function(args) {
-  this.id = (args['id'] === undefined) ? [] : args['id'];
-  this.features = (args['features'] === undefined) ? [] : args['features'];
-  this.endpoint = (args['endpoint'] === undefined) ? false : args['endpoint'];
-  this.intersection = (args['intersection'] === undefined) ? false : args['intersection'];
-  this.wayEnd = (args['wayEnd'] === undefined) ? false : args['wayEnd'];
-  this.wayIds = (args['wayIds'] === undefined) ? [] : args['wayIds'];  // deprecate
-  this.ways = (args['ways'] === undefined) ? [] : args['ways'];
-  this._coordinates = (args['coordinates'] === undefined) ? [] : args['coordinates'];
-  this.graph = (args['graph'] === undefined) ? [] : args['graph'];
+  this.id = (args.id === undefined) ? '0' : args.id;
+  this.features = (args.features === undefined) ? [] : args.features;
+  this.endpoint = (args.endpoint === undefined) ? false : args.endpoint;
+  this.intersection = (args.intersection === undefined) ? false : args.intersection;
+  this.wayEnd = (args.wayEnd === undefined) ? false : args.wayEnd;
+  this.wayIds = (args.wayIds === undefined) ? [] : args.wayIds;  // deprecate
+  this.ways = (args.ways === undefined) ? [] : args.ways;
+  this._coordinates = (args.coordinates === undefined) ? [] : args.coordinates;
+  this.coordinates = (args.coordinates === undefined) ? [] : args.coordinates;
+  this.graph = (args.graph === undefined) ? [] : args.graph;
 };
 
 Node.prototype.addWay = function (way) {
@@ -15,17 +16,18 @@ Node.prototype.addWay = function (way) {
     this.ways[way.id] = way;
   }
   return this;
-}
+};
 
 Node.prototype.getCoordinates = function() {
   return this._coordinates.slice();
-}
+};
 
 Node.prototype.setCoordinates = function(newCoords) {
   this._coordinates = newCoords.slice();
+  this.coordinates = this._coordinates;
   return this;
-}
+};
 
 Node.prototype.neighbors = function() {
   return this.graph.neighbors(this.id);
-}
+};
