e953bc39ce6d838f5946da15abec00fc1c4ed025
Seren <git@seren.net>
2015-12-02 14:59:36 -0800

    add debug method to check edge ordering in ways

--- js/illumap-way.js
+++ js/illumap-way.js
@@ -49,4 +49,60 @@ var Way = function(args) {
     return true;
   };
 
+
+  // check to make sure that way edges are ordered
+  this.checkEdgesOrdered = function checkEdgesOrdered() {
+    var endEdges = [];
+    var endNodes = [];
+    var nonEndNodes = [];
+    var ordered = [];
+    var nodes;
+    var i;
+    for (i = self.edges.length - 1; i >= 0; i--) {
+      nodes = self.edges[i].getNodes();
+      if (nodes[0].endpoint) {
+        endEdges.push(self.edges[i]);
+        endNodes.push(nodes[0]);
+        nonEndNodes.push(nodes[1]);
+      }
+      if (nodes[1].endpoint) {
+        endEdges.push(self.edges[i]);
+        endNodes.push(nodes[1]);
+        nonEndNodes.push(nodes[0]);
+      }
+    }
+    if (endEdges.length !== 2) debugger;
+
+    // prev - curr - next
+
+    ordered.push(endEdges[0]);
+    var nextNode = nonEndNodes[0];
+    var currNode = endNodes[0];
+    var prevNode;
+    while (ordered.length < self.edges.length) {
+      if (nextNode.endpoint) debugger; // we shouldn't hit an endpoint since the loop should exit before then
+      if (nextEdge === endEdges[1]) debugger; // we should quit before we hit this
+      if (nextNode.neighbors().length !== 2) debugger; // we shouldn't hit a node with more than 2 neighbors
+      prevNode = currNode;
+      currNode = nextNode;
+      nextNode = currNode.neighbors().filter(function (n) { return n !== prevNode; })[0];
+      if (nextNode === undefined) debugger;
+      nextEdge = self.graph.getEdge([currNode,nextNode]);
+      ordered.push(nextEdge);
+    }
+
+    // we now have an ordered set of edges. compare to the normal edge list and see if it's the same (back or forward)
+    var len = self.edges.length;
+    if (ordered[0] === self.edges[0]) {
+      for (i = 0; i < len; i++) {
+        if (self.edges[i] !== ordered[i]) debugger;
+      }
+    } else {
+      for (i = 0; i < len; i++) {
+        if (self.edges[i] !== ordered[len - i - 1]) debugger;
+      }
+    }
+    console.log("everything looks ok");
+  };
+
 };
