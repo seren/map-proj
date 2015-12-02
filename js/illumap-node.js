62c701c5c091d4f1297e49838481bedf2f0e8d81
Seren <git@seren.net>
2015-12-02 13:56:26 -0800

    add debugId to node (an easier-to-read uid)

--- js/illumap-node.js
+++ js/illumap-node.js
@@ -16,6 +16,22 @@ var Node = function(args) {
 
   if (this.graph === undefined) debugger;
 
+  // returns a hash of the id. much easier to read when debugging
+  this.debugId = function debugId() {
+    // from http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+    function hashCode(str){
+      var hash = 0;
+      if (str.length == 0) return hash;
+      for (i = 0; i < str.length; i++) {
+        char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+      }
+      return hash.toString(16);
+    }
+    return hashCode(self.id);
+  }
+
   this.getEdges = function getEdges() {
     return self._edges;
   };
